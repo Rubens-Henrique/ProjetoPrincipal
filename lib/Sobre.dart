6bcf3fd2a7ffe8d477d92d15a7e4f8721b8f34b8
Marcos Victor <marcosvictormelo@gmail.com>
2023-10-02 16:09:40 -0300

    imagem tela inicial e alteração do sobre nos

--- lib/Sobre.dart
+++ lib/Sobre.dart
@@ -8,35 +8,28 @@ class Sobre extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Sobre nós"),
-      ),
       body: Center(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Text(
-              "UAI, GÔ",
-              style: TextStyle(
-                  fontSize: 40,
-                  fontWeight: FontWeight.bold,
-                  fontFamily: 'PermanentMarker-Regular'),
-            ),
-            Text(
               "\nNossa missão:",
-              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+              style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 30,
+                  color: Colors.blue),
             ),
             Text(
               "\nLevar o turismo de uma maneira divertida!",
-              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
             ),
             Text(
-              "\n\nPara mais informações acesse nosso site: \nuaigo.com.br"
-              "\nOu clique no botão abaixo para obter nossos contatos",
-              style: TextStyle(fontSize: 20),
+              "\nPara mais informações acesse nosso site: uaigo.com.br"
+              "\n Ou clique no botão abaixo para obter nossos contatos",
+              style: TextStyle(fontSize: 14),
             ),
             Image.asset("assets/images/Logo.png",
-                width: 350, height: 300, fit: BoxFit.cover),
+                width: 283, height: 243, fit: BoxFit.cover),
             ElevatedButton(
                 child: Text('AJUDA'),
                 onPressed: () => print("Telefone de contato : 31 92382 47232 "
