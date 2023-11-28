963f0c707fc00168ad322fea6db081c1f8a83614
Rubens-Henrique <128429640+Rubens-Henrique@users.noreply.github.com>
2023-11-27 22:18:52 -0300

    Update pontoturistico_controller.dart
    Correção Erros

--- lib/Controllers/pontoturistico_controller.dart
+++ lib/Controllers/pontoturistico_controller.dart
@@ -1,100 +1,72 @@
 import 'dart:async';
-import 'dart:ffi';
+import 'dart:typed_data';
+import 'dart:ui' as ui;
+
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import 'package:geolocator/geolocator.dart';
-import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class PontoTuristicoController extends GetxController {
   final latitude = 0.0.obs;
   final longitude = 0.0.obs;
   StreamSubscription<Position> positionStream;
-  
 
-
-static PontoTuristicoController get to -> Get.find<PontoTuristicoController>();
-watchPosicao() async {
-    positionStream = Geolocator.getPositionStream().listen((Position position ) 
-{
-      if (position != null )
-      {
-         latitude.value= position.latitude;
-           longitude.value= position.longitude
- }
-
-});
-
-
-}
- 
- @override
- void onClose()
-{ positionStream.cancel();
-super.onClose();
-
-
-}
-
-
-Future<Position> _posicaoAtual() async {
-
-  LocationPermission permissao;
-  bool ativado = await Geolocator.isLocationServiceEnabled();
-
-  if(!ativado) 
-  { 
-    return Future.error( 'Por favor habilite a localização no celular' );
+  static PontoTuristicoController get to =>
+      Get.find<PontoTuristicoController>();
+  watchPosicao() async {
+    positionStream = Geolocator.getPositionStream().listen((Position position) {
+      if (position != null) {
+        latitude.value = position.latitude;
+        longitude.value = position.longitude;
+      }
+    });
   }
-  permissao= await Geolocator.checkPermission();
 
-  if(permissao = LocationPermission.denied) {
+  @override
+  void onClose() {
+    positionStream.cancel();
+    super.onClose();
+  }
 
-    permissao= await Geolocator.requestPermission();
+  Future<Position> _posicaoAtual() async {
+    LocationPermission permissao;
+    bool ativado = await Geolocator.isLocationServiceEnabled();
 
-    if (permissao = LocationPermission.denied) {
-      return Future.error('Voce precisa autorizar o acesso a localização ');
+    if (!ativado) {
+      return Future.error('Por favor habilite a localização no celular');
     }
-}
-    
-    if(permissao = LocationPermission.deniedForever) {
+    permissao = await Geolocator.checkPermission();
 
+    if (permissao == LocationPermission.denied) {
+      permissao = await Geolocator.requestPermission();
+
+      if (permissao == LocationPermission.denied) {
+        return Future.error('Voce precisa autorizar o acesso a localização ');
+      }
+    }
+
+    if (permissao == LocationPermission.deniedForever) {
       return Future.error('Autorize o acesso à localização nas configurações ');
-
     }
     return await Geolocator.getCurrentPosition();
-
   }
 
-getPosicao( ) async { 
-
-try { 
-
-final posicao = await _posicaoAtual();
-latitude.value= posicao.latitude;
-longitude.value = posicao.longitude;
-
-
+  getPosicao() async {
+    try {
+      final posicao = await _posicaoAtual();
+      latitude.value = posicao.latitude;
+      longitude.value = posicao.longitude;
+    } catch (e) {
+      Get.snackbar(
+        'Erro',
+        e.toString(),
+        backgroundColor: Colors.grey[900],
+        colorText: Colors.white,
+        snackPosition: SnackPosition.BOTTOM,
+      );
+    }
+  }
 }
-
-
-
-catch (e) { 
-
-get.snackBar( 
-'Erro',
-e.toString(), 
-backgroundColor: Colors.grey[900],
-colorText: Color.white,
-snackPosition: SnackPosition.BOTTOM,
-
-
-);
-
-
-}
-
-
-
-
-}
-}
-
