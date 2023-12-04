61102e05ff8cec69c55c41da724c9bf809764a15
Rubens <rubenshenrique456@outlook.com>
2023-12-04 14:55:49 -0300

    Atualizaçoes 1/X? mapa

---
+++ uaigo-main/lib/Controllers/pontoturistico_controller.dart
@@ -0,0 +1,112 @@
+import 'dart:async';
+import 'dart:typed_data';
+import 'dart:ui' as ui;
+import 'package:example/repositorios/pontos_repositorios.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:get/get.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+class PontoTuristicoController extends GetxController {
+  final latitude = 0.0.obs;
+  final longitude = 0.0.obs;
+  late StreamSubscription<Position> positionStream;
+  LatLng _position = LatLng(-23.571505, -46.689104);
+  late GoogleMapController _mapsController;
+    final markers = Set<Marker>();
+
+
+  static PontoTuristicoController get to =>
+      Get.find<PontoTuristicoController>();
+
+  get mapsController => _mapsController;
+  get position => _position
+
+  onMapCreated(GoogleMapController gmc) async  { 
+    _mapsController= gmc; 
+    getPosicao();
+    loadPonto();
+  }
+
+
+ loadPontos() {
+    final ponto = PontosRepositorios().pontos;
+    ponto.forEach((ponto) async {
+      markers.add(
+        Marker(
+          markerId: MarkerId(MarkerId),
+          position: LatLng(MarkerId.latitude, MarkerId.longitude),
+          icon: await BitmapDescriptor.fromAssetImage(
+            ImageConfiguration(),
+            'images/turistico.png',
+          ),
+          onTap: () => {
+            showModalBottomSheet(
+              context: appKey.currentState!.context,
+              builder: (context) => PontosDetalhes(ponto: ponto),
+            )
+          },
+        ),
+      );
+    });
+  }
+  
+
+  watchPosicao() async {
+    positionStream = Geolocator.getPositionStream().listen((Position position) {
+      if (position != null) {
+        latitude.value = position.latitude;
+        longitude.value = position.longitude;
+       
+      }
+    });
+  }
+
+  @override
+  void onClose() {
+    positionStream.cancel();
+    super.onClose();
+  }
+
+  Future<Position> _posicaoAtual() async {
+    LocationPermission permissao;
+    bool ativado = await Geolocator.isLocationServiceEnabled();
+
+    if (!ativado) {
+      return Future.error('Por favor habilite a localização no celular');
+    }
+    permissao = await Geolocator.checkPermission();
+
+    if (permissao == LocationPermission.denied) {
+      permissao = await Geolocator.requestPermission();
+
+      if (permissao == LocationPermission.denied) {
+        return Future.error('Voce precisa autorizar o acesso a localização ');
+      }
+    }
+
+    if (permissao == LocationPermission.deniedForever) {
+      return Future.error('Autorize o acesso à localização nas configurações ');
+    }
+    return await Geolocator.getCurrentPosition();
+  }
+
+  getPosicao() async {
+    try {
+      final posicao = await _posicaoAtual();
+      latitude.value = posicao.latitude;
+      longitude.value = posicao.longitude;
+        _mapsController.animateCamera();
+         CameraUpdate.newLatLng(LatLng(latitude.value,latitude.value));
+    } catch (e) {
+      Get.snackbar(
+        'Erro',
+        e.toString(),
+        backgroundColor: Colors.grey[900],
+        colorText: Colors.white,
+        snackPosition: SnackPosition.BOTTOM,
+      );
+    }
+  }
+}
---
+++ uaigo-main/lib/pages/pontosturistico_page.dart
@@ -0,0 +1,78 @@
+import 'package:example/Controllers/pontoturistico_controller.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+
+
+
+final appKey = GlobalKey();
+
+
+class PontosturisticosPage extends StatelessWidget {
+  PontosturisticosPage({Key key}) : super(key: key);
+
+  filtro() {
+    return SimpleDialog(
+      title: Text('Filtrar por Proximidade'),
+      children: [
+        Obx(
+          () => Slider(
+            value: PontoTuristicoController.to.raio.value,
+            min: 0,
+            max: 10,
+            divisions: 10000,
+            label: PontoTuristicoController.to.distancia,
+            onChanged: (value) => PontoTuristicoController.to.raio.value = value,
+          ),
+        ),
+        Padding(
+          padding: EdgeInsets.only(right: 24, top: 24),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              ElevatedButton(
+                onPressed: () => PontoTuristicoController.find.to.filtrarCafes(),
+                child: Text('Filtrar'),
+              ),
+              TextButton(onPressed: () => Get.back(), child: Text('Cancelar')),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final controller = Get.put(PontosturisticosPage());
+
+    return Scaffold(
+       key: appKey,
+      appBar: AppBar(
+        title: Text('Pontos turísticos'),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.filter_list),
+            onPressed: () {
+              showDialog(context: context, builder: (context) => filtro());
+            },
+          ),
+        ],
+      ),
+      body: GetBuilder<PontoTuristicoController>(
+        init: controller,
+        builder: (value) => GoogleMap(
+          mapType: MapType.normal,
+          zoomControlsEnabled: true,
+          initialCameraPosition: CameraPosition(
+            target: controller.position,
+            zoom: 13,
+          ),
+          onMapCreated: controller.onMapCreated,
+          myLocationEnabled: true,
+          markers: controller.markers,
+        ),
+      ),
+    );
+  }
